d661007b0e275048583035952107feb35f2ede67
Bruno Pedro <bpedro@tarpipe.com>
2010-12-15 18:23:30 +0000

    An example usage of the library.

---
+++ example.js
@@ -0,0 +1,27 @@
+//
+// Require our fs lib, not the original.
+//
+var fs = require('./lib/fs.js');
+
+//
+// Example with non-recursion.
+//
+fs.mkdir('example_dir/first/second/third/fourth/fifth', 0777, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Directory created');
+    }
+});
+
+//
+// Example with recursion -- notice the parameter
+// right before the callback function.
+//
+fs.mkdir('example_dir/first/second/third/fourth/fifth', 0777, true, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Directory created');
+    }
+});
